b0f1ba670ae7d5c651095d22ff6c09c24677ece2
Eduard Anisonyan <e.anisonyan@superjob.ru>
2018-03-28 20:40:50 +0300

    FitsSystemWindows added

--- app/src/main/java/com/example/bug/coordinatordemo/ScrollingActivity.java
+++ app/src/main/java/com/example/bug/coordinatordemo/ScrollingActivity.java
@@ -6,61 +6,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    ImageView cat;
-    boolean show;
+	ImageView cat;
+	boolean show;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scrolling);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        final FrameLayout vg = (FrameLayout) findViewById(R.id.temp);
+		final FrameLayout vg = findViewById(R.id.temp);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(view -> {
 
-                AnimationUtils.setVisibilityWithAnim(show, vg);
-                show = !show;
+			AnimationUtils.setVisibilityWithAnim(show, vg);
+			show = !show;
 
-            }
-        });
+		});
 
-        cat = (ImageView) findViewById(R.id.cat);
+		cat = findViewById(R.id.cat);
 
-        AnimationUtils.setVisibilityWithAnim(!show, vg);
+		AnimationUtils.setVisibilityWithAnim(!show, vg);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+		return true;
+	}
 
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
